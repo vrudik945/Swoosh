0b62d4298783bf4777ab7d2380969a5b627f0ebe
vrudik945 <Zanzibar12$>
2020-08-24 15:05:31 +0300

    Third screen

--- app/src/main/java/com/v_rudik/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/v_rudik/swoosh/LeagueActivity.kt
@@ -1,11 +1,18 @@
 package com.v_rudik.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    fun leagueNxtClicked(view: View) {
+        val skillActivity = Intent (this, SkillActivity::class.java)
+        startActivity(skillActivity)
     }
 }
---
+++ app/src/main/java/com/v_rudik/swoosh/SkillActivity.kt
@@ -0,0 +1,11 @@
+package com.v_rudik.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SkillActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill)
+    }
+}
